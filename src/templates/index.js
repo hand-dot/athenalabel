a213f911178cef70c84bfcd7883ac2008f2af065
hand-dot <kyouhei.fukuda0729@gmail.com>
2018-11-12 02:17:30 +0900

    add some template data

--- src/templates/index.js
+++ src/templates/index.js
@@ -2,10 +2,17 @@ import レターパック from './datas/レターパック.json';
 import 宛名8面 from './datas/宛名8面.json';
 import 出品者向けラベル24面 from './datas/出品者向けラベル24面.json';
 import スマートレター from './datas/スマートレター.json';
+import ストライプ24面 from './datas/ストライプ24面.json';
+import フレーム24面 from './datas/フレーム24面.json';
+import ライン24面 from './datas/ライン24面.json';
+
 
 export default {
   宛名8面,
   出品者向けラベル24面,
+  ライン24面,
+  フレーム24面,
+  ストライプ24面,
   レターパック,
   スマートレター,
 };
