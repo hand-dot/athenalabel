3c85eeb9ef9c6a3aecd92f83625a59e14bae60d8
hand-dot <kyouhei.fukuda0729@gmail.com>
2018-11-12 04:43:28 +0900

    minor fix

--- src/templates/index.js
+++ src/templates/index.js
@@ -4,15 +4,15 @@ import 装飾なしラベル24面 from './datas/装飾なしラベル24面
 import スマートレター from './datas/スマートレター.json';
 import ストライプラベル24面 from './datas/ストライプラベル24面.json';
 import フレームラベル24面 from './datas/フレームラベル24面.json';
-import ラインラベル24面 from './datas/ラインラベル24面.json';
+import シンプルラベル24面 from './datas/シンプルラベル24面.json';
 
 
 export default {
   宛名8面,
-  装飾なしラベル24面,
-  ラインラベル24面,
+  シンプルラベル24面,
   フレームラベル24面,
   ストライプラベル24面,
+  装飾なしラベル24面,
   レターパック,
   スマートレター,
 };
